29084d492725392af633180be91678388015aa14
Haneen Abu-Khater <abukhater.haneen@gmail.com>
2017-11-09 16:15:57 -0800

    last push

--- app/src/main/java/com/epicodus/coffeeshop/CoffeeShopActivity.java
+++ app/src/main/java/com/epicodus/coffeeshop/CoffeeShopActivity.java
@@ -3,11 +3,16 @@ package com.epicodus.coffeeshop;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class CoffeeShopActivity extends AppCompatActivity {
 
     private TextView mLocationTextView;
+    private ListView mListView;
+    private String[] coffeeShops = new String[] {"Spella", "Starbucks",
+            "Public Domain", "Water Avenue"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +24,9 @@ public class CoffeeShopActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
         mLocationTextView.setText("Here are all the coffee shops near: " + location);
+
+        mListView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, coffeeShops);
+        mListView.setAdapter(adapter);
     }
 }
